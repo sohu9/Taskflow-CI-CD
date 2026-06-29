ecb88a5ff0f40821f72e5de834a9d657374b86c1
sohu9 <ansarishoaib5252@gmail.com>
2026-06-29 15:56:53 +0530

    Taskflow done

---
+++ app.js
@@ -0,0 +1,86 @@
+const express = require('express'),
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override'),
+    sanitizer = require('sanitizer'),
+    app = express(),
+    port = process.env.PORT || 8000;
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+// https: //github.com/expressjs/method-override#custom-logic
+app.use(methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        // look in urlencoded POST bodies and delete it
+        let method = req.body._method;
+        delete req.body._method;
+        return method
+    }
+}));
+
+
+let todolist = [];
+
+/* The to do list and the form are displayed */
+app.get('/', function (req, res) {
+        res.render('todo.ejs', {
+            todolist,
+            clickHandler: "func1();"
+        });
+    })
+
+    /* Adding an item to the to do list */
+    .post('/add/', function (req, res) {
+        // Escapes HTML special characters in attribute values as HTML entities
+        let newTodo = sanitizer.escape(req.body.newtodo);
+        if (req.body.newtodo != '') {
+            todolist.push(newTodo);
+        }
+        res.redirect('/todo');
+    })
+
+    /* Deletes an item from the to do list */
+    .get('/delete/:id', function (req, res) {
+        if (req.params.id != '') {
+            todolist.splice(req.params.id, 1);
+        }
+        res.redirect('/todo');
+    })
+
+    // Get a single todo item and render edit page
+    .get('/edit/:id', function (req, res) {
+        let todoIdx = req.params.id;
+        let todo = todolist[todoIdx];
+
+        if (todo) {
+            res.render('edititem.ejs', {
+                todoIdx,
+                todo,
+                clickHandler: "func1();"
+            });
+        } else {
+            res.redirect('/todo');
+        }
+    })
+
+    // Edit item in the todo list 
+    .put('/edit/:id', function (req, res) {
+        let todoIdx = req.params.id;
+        // Escapes HTML special characters in attribute values as HTML entities
+        let editTodo = sanitizer.escape(req.body.editTodo);
+        if (todoIdx != '' && editTodo != '') {
+            todolist[todoIdx] = editTodo;
+        }
+        res.redirect('/todo');
+    })
+    /* Redirects to the to do list if the page requested is not found */
+    .use(function (req, res, next) {
+        res.redirect('/');
+    })
+
+    .listen(port, function () {
+        // Logging to console
+      console.log(`🚀 TaskFlow is running on http://localhost:${port}/todo`);
+    });
+// Export app
+module.exports = app;
